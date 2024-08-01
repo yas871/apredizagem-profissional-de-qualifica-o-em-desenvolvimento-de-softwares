686a8a4c3db41779c631a0eea51511b009bbed91
yas871 <yasminlenny855@gmail.com>
2024-08-01 17:40:54 -0300

    Add files via upload

---
+++ dados_cli.sql
@@ -0,0 +1,31 @@
+use bd_caso_estudos_vendas
+INSERT INTO `bd_caso_estudos_vendas`.`tb_cli`
+(`cli_cod`,`cli_nome`,`cli_cpf`,`cli_end_rua`,`cli_end_num`,`cli_end_bairro`,`cli_end_cep`)
+values
+('01','Hudson neves','Ruadas Acacias','100p','Centro','08000000','05588992236'),
+('02','Izabella Oliveira','Rua das neves','04','sul','01083798','06555598871'),
+('03','Emanuelle Candito','Rua morro azul','08','residencial oeste','72540276','25888888888'),
+('04','Yan fellipy','rua da agua','3','aguas escura','72000000','0552214535'),
+('05','Samuel sales Oliveira','rua do sol','4','aguas mornas','72000100','0112214110'),
+('7','Emilly gabrielly','rua do fogo','7','samambaia norte','72000333','23003782415'),
+('06','Luane gabrielly','rua das araras','21','residencial vila boa','98765444','98765432103'),
+('09','Yasmin lenny','rua capao','22','cerrado','88090000','93396628351'),
+('10','Beatriz Gomes','rua da Teresa','51',' Águas frias','7196253','07078855156'),
+('08','Nicolas Rabelo','Rua Ypiranga','6','vRiacho Fundo','717700102','03651035190'),
+('11','Luciel Carvalho Aguiar','Rua maranhão','12','setor de chácaras anhanquera c',' 72871667','71174239190'),
+('12','Rikelmy Zuza',' Rua Ypiranga','6','Riacho Fundo',' 717700102',' 03651035197'),
+('13','Nathan Gonçalves','rua São Gabriel','4','jardim vila','82000','01130568212'),
+('14','Maria Beatriz','rua das pedras','17','gama','71880683','07608801760'),
+('16','Ingrid','jardim','12','norte','90950023',' 04450280820'),
+('17','Maria Victoria','rua do fuzuê','50',' Planaltina','20009698','08036083069'),
+('18','Daniel Gomes','Gomes Passos','16','Norte','72245612','05759582172'),
+('19','Bruno Felipe','rua fortaleza','40','Valparaiso','7254540','03599213286'),
+('20','Pedro Alves','rua da pedra','33','cei norte','7254540','99999999999'),
+('21','Lara Rodrigues','Rua das flores','09','águas lindas','72610202','08670062181'),
+('22','Nathan de Aguiar (Jimmy)','Rua 3','03','SHVP','7250048','74589669845'),
+('17','Debora','rua da saudade','08','norte','7223890','889900221');
+
+select * from tb_cli;
+
+
+
